e09f2f3b1803f10a8c3e8ecdea22c31f2e0ccf1b
SCPassion <bernard.cryptohk@gmail.com>
2025-06-30 08:44:29 +0200

    in progress

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-  print("Starting Asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
 
   # Initialize the game window
   pygame.init()
 
   # Set up the display
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  
+  #Initialize a player object
+  player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+  # Set delta time for the game loop
+  clock = pygame.time.Clock()
+  dt = 0
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+      
     screen.fill((0, 0, 0))  # Fill the screen with black
+    player.draw(screen)  # Draw the player
+
     pygame.display.flip()  # Update the display
+    waited = clock.tick(60)  # Limit the frame rate to 60 FPS
+    dt = waited / 1000.0  # Convert milliseconds to seconds
 
 if __name__ == "__main__":
   main()
